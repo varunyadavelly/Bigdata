d550820f584843e2c0badde4937b067446455e8e
varunyadavelly <varunyadavelly28@gmail.com>
2023-12-19 16:37:50 -0800

    Ques 6 updated

--- src/main/scala/que6.scala
+++ src/main/scala/que6.scala
@@ -1,7 +1,24 @@
 object que6 {
   def main(args: Array[String]): Unit = {
+    print("Enter the first number: ")
+    val first = scala.io.StdIn.readDouble()
+
+    print("Enter the second number: ")
+    val sec = scala.io.StdIn.readDouble()
+
+    print("Enter the operator (+, -, *, /): ")
+    val op = scala.io.StdIn.readLine()
+
+    val ans = op match {
+      case "+" => first + sec
+      case "-" => first - sec
+      case "*" => first * sec
+      case "/" => if (sec != 0) first / sec else "Cannot divide by zero"
+      case _ => "Invalid operator"
+    }
+
+    println(s"Final Result: $ans")
 
   }
 }
-//Write a program to perform simple math based on the user inputs by using
-//Switch condition.(+ , - , * , /)
+//Write a program to perform simple math based on the user inputs by using Switch condition.(+ , - , * , /)
